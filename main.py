9e2dcfeb611771d86cc2861d51da0e5b8e22ed12
Jessica RAMANANTSOA <fifaliana.dev@gmail.com>
2021-03-25 13:02:38 +0100

    jdr

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-#initialisation du nombre de vie de mon player et de l'ennemi + nb de potions
-moi = 50
-ennemi = 50
-choix_possibles =["1","2"]
-#choix possibles
-while moi > 0 and ennemi >0:
-    choix  = input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) ?")
-    if choix not in choix_possibles:
-        print("Entrer un choix valide")
-#attaquer
-#prendre une potion
-#fin de partie
+import random
+
+player = 50
+ennemy = 50
+potions = 3
+menu =["1","2"]
+
+while player > 0 and ennemy > 0:
+    choice =input(" 1 Attack or 2  use potion ? ")
+    if choice == "2":
+        if potions == 0:
+            print("No more potions")
+        else:
+            player = player+random.randint(15,50)
+            potions = potions-1
+            print(f"HP after potion {player}")
+            #the ennemy attacks twice
+            player = player - random.randint(5,15)
+            #print(f"ennemy first attack :{player}")
+            player = player - random.randint(5,15)
+            #print(f"ennemy second attack :{player}")
+    if choice == "1":
+        ennemy = ennemy - random.randint(5,10)
+        player = player - random.randint(5,15)
+    print(f"PLayer : {player} / Ennemy : {ennemy}")
+    print(potions)
+    if player <=0:
+        print("You lost the game.")
+        break
+    if ennemy <=0:
+        print("You won the game.")
+        break
